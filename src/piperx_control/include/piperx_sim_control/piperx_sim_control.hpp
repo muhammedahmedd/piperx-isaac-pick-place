40f77b7a9a4fce386aea507ea1ed3ef43cccaf72
muhammedahmedd <mohamed.ahmed.1@vanderbilt.edu>
2026-05-28 17:47:27 -0500

    add MoveIt arm and gripper control members

--- src/piperx_control/include/piperx_sim_control/piperx_sim_control.hpp
+++ src/piperx_control/include/piperx_sim_control/piperx_sim_control.hpp
@@ -1,20 +1,37 @@
 #ifndef PIPERX_SIM_CONTROL__PIPERX_SIM_CONTROL_HPP_
 #define PIPERX_SIM_CONTROL__PIPERX_SIM_CONTROL_HPP_
 
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/joint_state.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
 
 class PiperXSimControl : public rclcpp::Node
 {
 public:
+
   PiperXSimControl();
 
-private:
-  void publish_joint_command();
-  void publish_scan_pose_once();
+  ~PiperXSimControl() = default;
 
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+  void mainJointMovement();
+
+  void moveArmJoints(const std::vector<double> & joint_angles);
+
+  void moveGripperJoints(const std::vector<double> & joint_angles);
+
+
+private:
+
+  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> arm_group_;
+
+  std::shared_ptr<moveit::planning_interface::MoveGroupInterface> gripper_group_;
+
+  moveit::planning_interface::MoveGroupInterface::Plan arm_plan_;
+
+  moveit::planning_interface::MoveGroupInterface::Plan gripper_plan_;
+
+  std::vector<double> scan_pose_joints_ = {0.0, 0.373, -1.283, 1.315, 0.0, 0.0};
+
+  std::vector<double> gripper_open_joints_ = {0.050, -0.050};
 };
 
 #endif 
